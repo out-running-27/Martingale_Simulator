b56d473a7deea6dc8385177cbb1599c9700220ed
Russum <nm178702@nm.org>
2018-01-30 14:46:08 -0600

    spelling error

--- martingale_simulator.py
+++ martingale_simulator.py
@@ -54,7 +54,7 @@ def play_game(starting_bet, life_savings, walk_away, color):
         game_number += 1
         
         if life_savings > walk_away:
-            print("Congradulations! You walked away in round {} "
+            print("Congratulations! You walked away in round {} "
                   "the largest bet you made this round was ${}"
                   .format(game_number, largest_bet))
             return life_savings
